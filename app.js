4fd3c79c28d1d8b8825f5cc3dbd885480d93fccd
Matheus Cruz <matheus.cruz.2004@hotmail.com>
2024-11-03 23:18:33 -0300

    commit

---
+++ app.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const apiUrl = "http://137.184.108.252:5000/api";
+    let token = "";
+
+    async function login() {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        console.log('Dados de login:', { email, password });
+
+        try {
+            const response = await fetch(`${apiUrl}/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, password }),
+            });
+
+            if (!response.ok) {
+                throw new Error("Falha no login");
+            }
+
+            const data = await response.json();
+            token = data.token;
+            alert("Login realizado");
+
+            document.getElementById("token").innerText = `Último Token: ${token}`;
+
+            fetchCidades();
+        } catch (error) {
+            alert("Erro ao fazer login.");
+        }
+    }
+
+    async function fetchCidades() {
+        try {
+            const response = await fetch(`${apiUrl}/cidades`, {
+                method: "GET",
+                headers: {
+                    "x-access-token": token,
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error("Erro ao buscar cidades.");
+            }
+
+            const cidades = await response.json();
+            const tableBody = document.querySelector('#cidades tbody');
+
+            tableBody.innerHTML = '';
+
+            cidades.forEach(city => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${city.id}</td>
+                    <td>${city.nome}</td>
+                `;
+                tableBody.appendChild(row);
+            });
+        } catch (error) {
+            alert("Erro ao buscar cidades.");
+        }
+    }
+
+    document.getElementById("login").addEventListener("click", login);
+});
